77abbdbfb51922c343db3c25c2750bdb5115605a
Akash Chowdhury <akashbumbac24@gmail.com>
2025-07-07 18:25:16 +0530

    "Removed import statements and commented line in App.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-import Sidebar from "./component/sidebar";
 // In your index.js or App.js
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
